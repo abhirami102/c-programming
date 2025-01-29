c6caaa4a8c21c0cc9f80d2a31748bd28772d651d
Abhirami <137210545+abhirami102@users.noreply.github.com>
2025-01-29 16:19:28 +0530

    Add files via upload

---
+++ age.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+int n;
+printf("Enter your age:");
+scanf("%d",&n);
+if (n>=21)
+{
+printf("Congratulation! You are eligible for casting your vote.");}
+else {
+printf(" You are not eligible for casting your vote.");}
+return 0;
+}

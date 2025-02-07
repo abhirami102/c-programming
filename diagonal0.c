ae23b0593b735dc1fee8ffdeb60d2ba671a388da
Abhirami <137210545+abhirami102@users.noreply.github.com>
2025-02-07 09:48:38 +0530

    Add files via upload

--- diagonal0.c
+++ diagonal0.c
@@ -9,7 +9,15 @@ int main(){
   for(j=0;j<cols;j++){
    scanf("%d",&array[i][j]);
    }
-  }
+  } 
+  int sum=0;
+  for (i=0;i<rows;i++){
+   for(j=0;j<cols;j++){
+    if(i==j){
+     array[i][j]=0;
+ }}}
+ printf("\nthe sum of the diagonal elements are:%d",sum);
+  printf("\n");
   for (i=0;i<rows;i++){
    for(j=0;j<cols;j++){
     printf("%d\t",array[i][j]);

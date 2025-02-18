ec15070d46ff058a4ec332b023b6551b84dd559a
Abhirami <137210545+abhirami102@users.noreply.github.com>
2025-02-19 00:25:46 +0530

    Add files via upload

---
+++ string_rev.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+    printf("\nEnter your word:");
+    char str[100];
+    fgets(str,sizeof(str),stdin);
+    int length;
+    for(int i=0;str[length]!='\0';length++);
+    int i=length-1;
+    for (int j=0;j<length/2;j++){
+        int temp=str[i];
+        str[i]=str[j];
+        str[j]=temp;
+        i--;
+    }
+    printf("\nThe reverse of your word is:");
+    puts(str);
+    return 0;
+
+}

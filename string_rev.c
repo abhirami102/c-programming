f9f58a3c4904f7668f557a721b6b8748bc1ed1d5
Abhirami <137210545+abhirami102@users.noreply.github.com>
2025-02-19 00:29:52 +0530

    Update string_rev.c

--- string_rev.c
+++ string_rev.c
@@ -4,7 +4,7 @@ int main(){
     char str[100];
     fgets(str,sizeof(str),stdin);
     int length;
-    for(int i=0;str[length]!='\0';length++);
+    for(int length=0;str[length]!='\0';length++);
     int i=length-1;
     for (int j=0;j<length/2;j++){
         int temp=str[i];
